c72ebaeb559adf0b1d12e56b8082de242d87a95a
Andri Firdaus <andricrafter007@gmail.com>
2022-09-21 10:50:28 +0800

    membuat dan mendaftarkan notes plugin

--- src/server.js
+++ src/server.js
@@ -1,18 +1,14 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notesPlugin = require('./notesPlugin');
 
 const init = async () => {
-  const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
-  });
+  const server = Hapi.server();
 
-  server.route(routes);
+  // Registrasi plugin
+  await server.register({
+    plugin: notesPlugin,
+    options: { notes: [] },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
